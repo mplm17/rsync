5c57a5db858e2bd4f5134cc6bf9f64e6dcca42ec
mplm17 <39955446+mplm17@users.noreply.github.com>
2023-01-17 23:43:47 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,12 @@ echo "################################################################"
 echo "$(date) - Loading rsync..."
 echo "################################################################"
 echo "Environment variables:"
-
 echo "CRON_EXPRESSION=${CRON_EXPRESSION}"
+RSYNCVARS=( $(compgen -A variable | grep "^RSYNC") )
+for RSYNCVAR in RSYNCVARS; do
+   echo "RSYNCVAR=${RSYNCVAR}"
+   echo "rsync ${RSYNCVAR}" >> /rsync.sh
+done
 echo "################################################################"
 printenv > /var/spool/cron/crontabs/root
 echo "${CRON_EXPRESSION} /rsync.sh >> /rsync.log 2>&1" >> /var/spool/cron/crontabs/root
