2e9c895f5cdc50cc6175e96a23af9713dd5e12f4
mplm17 <39955446+mplm17@users.noreply.github.com>
2023-01-18 11:21:14 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,12 @@ echo "CRON_EXPRESSION=${CRON_EXPRESSION}"
 RSYNCVARS=$(compgen -A variable | grep "^RSYNC")
 for RSYNCVAR in $RSYNCVARS; do
    echo "${RSYNCVAR}=${!RSYNCVAR}"
-   echo 'echo "Start '${RSYNCVAR}' with command: rsync '${!RSYNCVAR}'"' >> /rsync.sh
+   echo 'echo "Start $(date) - '${RSYNCVAR}' with command: rsync '${!RSYNCVAR}'"' >> /rsync.sh
    echo "rsync ${!RSYNCVAR}" >> /rsync.sh
-   echo 'echo "'${RSYNCVAR}' has ended."' >> /rsync.sh
+   echo 'echo "$(date) - '${RSYNCVAR}' has ended."' >> /rsync.sh
 done
-echo "################################################################"
 echo 'echo "$(date) - rsync.sh finished..."' >> rsync.sh
+echo "################################################################"
 printenv > /var/spool/cron/crontabs/root
 echo "${CRON_EXPRESSION} /rsync.sh >> /rsync.log 2>&1" >> /var/spool/cron/crontabs/root
 crontab /var/spool/cron/crontabs/root
